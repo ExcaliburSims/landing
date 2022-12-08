33c23a4e25435d30a3109af4f2ae4db0cdbda796
ExcaliburSims <ximeonzilu@gmail.com>
2022-12-08 21:43:21 +0100

    fix condition of disponibility of course

--- src/components/Card-formation.tsx
+++ src/components/Card-formation.tsx
@@ -1,29 +1,29 @@
 import "./Card-formation.css";
-import LaptopIcon from "./assets/svg/laptop.svg";
 import PlayIcon from "./assets/svg/play.svg";
 
-const Card = () => {
+const Card = (props: any) => {
+  const linked = false;
   return (
     <div className="overlay">
       <div className="square ">
         <div className="d-flex align-items-end flex-column card-content">
           <div className="icon-container">
-            <img src={LaptopIcon} alt="Laptop icon" className="card-icon" />
+            <img src={props.icon} alt="Laptop icon" className="card-icon" />
           </div>
           <div>
-            <h2 className=" card-title">Responsive Web Design</h2>
-            <p className="mt-4 card-text">
-              Dans cette formation, vous apprendrez les langages que les
-              développeurs utilisent pour créer des pages Web : HTML (Hypertext
-              Markup Language) pour le contenu, et CSS (Cascading Style Sheets)
-              pour la conception. Enfin, vous apprendrez à créer des pages Web
-              adaptées à différentes tailles d'écran en créant
-            </p>
+            <h2 className=" card-title">{props.title}</h2>
+            <p className="mt-4 card-text">{props.paragraph}</p>
           </div>
         </div>
         <div className="text-end pt-5 link-container">
-          <span className="card-link pe-3">Suivre le cours</span>
-          <img src={PlayIcon} alt="Laptop icon" className="play" />
+          {linked !== false ? (
+            <>
+              <span className="card-link pe-3">Suivre le cours</span>
+              <img src={PlayIcon} alt="Laptop icon" className="play" />
+            </>
+          ) : (
+            <span className="text-muted card-link">Bientôt disponible</span>
+          )}
         </div>
       </div>
     </div>
--- src/components/Section-formation.tsx
+++ src/components/Section-formation.tsx
@@ -2,6 +2,15 @@ import "./Section-formation.css";
 import Card from "./Card-formation";
 import Startop from "./assets/svg/startop.svg";
 import Starbottom from "./assets/svg/starbottom.svg";
+import LaptopIcon from "./assets/svg/laptop.svg";
+import AlgoIcon from "./assets/svg/algorithmIcon.svg";
+
+const title1 = "Responsive Web Design";
+const title2 = "JavaScript Algorithms and Data Structures";
+const paragraph1 =
+  "Dans cette formation, vous apprendrez les langages que les développeurs utilisent pour créer des pages Web : HTML (Hypertext Markup Language) pour le contenu, et CSS (Cascading Style Sheets) pour la conception. Enfin, vous apprendrez à créer des pages Web adaptées à différentes tailles d'écran en créant";
+const paragraph2 =
+  "Alors que HTML et CSS contrôlent le contenu et le style d'une page, JavaScript est utilisé pour la rendre interactive. Dans le cadre de la certification JavaScript Algorithm and Data Structures, vous apprendrez les principes fondamentaux de JavaScript, etc.";
 
 const Section = () => {
   return (
@@ -16,8 +25,8 @@ const Section = () => {
           </p>
         </div>
         <div className="h-75 card-container">
-          <Card />
-          <Card />
+          <Card icon={LaptopIcon} title={title1} paragraph={paragraph1} />
+          <Card icon={AlgoIcon} title={title2} paragraph={paragraph2} />
         </div>
       </div>
       <img src={Starbottom} alt="stars" className="stars-bottom" />
